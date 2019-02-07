49be25f1e99d73fe43999d1b3b69d2e7f9988bf4
limyluy <luisroserodesarrollador@gmail.com>
2019-02-07 16:08:17 -0500

    adicionamos algo

--- app/src/main/java/com/allisonapps/MainActivity.java
+++ app/src/main/java/com/allisonapps/MainActivity.java
@@ -7,6 +7,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // para realizar el splashScrem seteamos el Style Appthem.SpalshScremm en el Manifes
+        // con la liena de codigo abajo volvemos al tema original de la aplicacion
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
